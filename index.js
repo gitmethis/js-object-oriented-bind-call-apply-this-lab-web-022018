b0093076b2fbc6e6b3497cf4df72a11ec7449e05
Chase <chaseit12@gmail.com>
2018-04-04 16:41:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function setThisWithCall(fn, thisValue, arg){
 }
 
 function setThisWithApply(fn, thisValue, arg){
-  return fn.apply(thisValue, [arg])
+  return fn.apply(thisValue, arg)
 }
 
 function returnNewFunction(functionToBeCopied, thisValue){
